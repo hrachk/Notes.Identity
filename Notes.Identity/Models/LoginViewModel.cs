2e9e3d23ff10371d6bac1edc72f4bdb38d5e9efc
Hrach <hkarap@hotmail.com>
2021-11-11 11:52:14 +0400

    логин и регистрация пользователя Created MVC , style

---
+++ Notes.Identity/Models/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Notes.Identity.Controllers
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+
+    }
+}
